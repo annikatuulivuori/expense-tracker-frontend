648e6732d8953e0656377633bfa17f7a3c13e456
annika <atuulivuo@gmail.com>
2024-01-27 20:13:44 +0200

    lisää ExpenseList -komponentti

---
+++ src/components/ExpenseList.tsx
@@ -0,0 +1,37 @@
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
+
+const ExpenseList: React.FC = () => {
+  const [expenses, setExpenses] = useState([])
+  const baseUrl = 'http://localhost:8080/api/expenses'
+
+  useEffect(() => {
+    const fetchExpenses = async () => {
+      try {
+        const response = await axios.get(baseUrl + '/viewExpenses')
+        setExpenses(response.data)
+        console.log('Expenses data: ', response.data)
+      } catch (error) {
+        console.log('Error fetching expenses: ', error)
+      }
+    }
+
+    fetchExpenses()
+  }, [])
+
+
+  return (
+    <div>
+      <h2>All expenses: </h2>
+      <ul>
+        {expenses.map((expense: any) => (
+          <li key={expense.id}>
+            {`Description: ${expense.desc}, Amount: $${expense.amount}`}
+          </li>
+        ))}
+      </ul>
+    </div>
+  )
+}
+
+export default ExpenseList
