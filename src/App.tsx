d9c76f9c8f89b0e5139e2bbf0fb9cd61385043e5
annika <atuulivuo@gmail.com>
2024-01-27 22:28:28 +0200

    lisää App.tsx -tiedostoon ExpenseForm ja avainsana sen käyttämistä varten

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import './App.css';
 import Expense from './components/Expense';
+import ExpenseForm from './components/ExpenseForm';
 import expenseService from './services/expense'
 import type { ExpenseProps } from './components/Expense'
 
@@ -13,9 +14,16 @@ const App: React.FC = () => {
       .catch(error => console.error('Error fetching expenses:', error));
   }, []);
 
+  const handleExpenseSubmit = (newExpense: ExpenseProps) => {
+    expenseService.addExpense(newExpense).then(() => {
+      setExpenses(prevExpense => [...prevExpense, newExpense])
+    }).catch(error => console.error('Error adding expense:', error))
+  }
+
   return (
     <div>
       <h1>Expense Tracker</h1>
+      <ExpenseForm onExpenseSubmit={handleExpenseSubmit} />
       {expenses.length > 0 ? (
         expenses.map(expense => <Expense key={expense.id} id={expense.id} desc={expense.desc} amount={expense.amount} />)
       ) : (
