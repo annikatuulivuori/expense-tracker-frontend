f06f77f32b754fbdce2517ef121e362d400fc877
annika <atuulivuo@gmail.com>
2024-01-27 20:16:03 +0200

    lisää ExpenseList App.tsx-tiedostoon

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
+import ExpenseList from './components/ExpenseList';
 
 const App: React.FC = () => {
   return (
     <div>
       <h1>Expense Tracker</h1>
+      <ExpenseList />
     </div>
   );
 }
